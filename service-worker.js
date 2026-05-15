bcd70c4e95e1ab69d04c599d336f084f0e07322d
hezhouyf4263507 <670616767@qq.com>
2026-05-15 13:27:24 +0800

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'gerencangku-v1';
+const urlsToCache = [
+  '/gerencangku/',
+  '/gerencangku/index.html',
+  '/gerencangku/manifest.json'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200) return response;
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, responseToCache));
+            return response;
+          });
+      })
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
